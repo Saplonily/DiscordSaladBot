c98ae504563a510ab437d07018267df76197cc21
Saplonily <Saplonily@outlook.com>
2022-08-04 19:58:15 +0800

    now salad can be built normally, but without any functions

---
+++ AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyCompany("Saplonily")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("DiscordSaladBot")]
+[assembly: AssemblyTitle("DiscordSaladBot")]
--- Source/CommandAnalyzer.cs
+++ Source/CommandAnalyzer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Discord.WebSocket;
 
 public static class CommandAnalyzer
@@ -25,18 +26,62 @@ public static class CommandAnalyzer
         //遍历公共指令集
         foreach (var set in RootPublicCommandSet)
         {
-            
+            //depth是
+            int depth = -1;
+            Command cmdFound = null;
+            depth = FindCommandInSet(set, ref cmdParts, ref message, out cmdFound);
+            //合并多余参数,+2因为depth指向集合参数位置,要右移指令位后再移到参数0位
+            CombineArgsParts(ref cmdParts, depth + 2);
+            cmdFound.Action.Invoke(cmdParts[(depth + 1)..], message);
         }
+
     }
-        //var args = new string[cmd.ArgsCounts];
-        //Array.Copy(parts, 2, args, 0, args.Length);
-        //cmd.Action.Invoke(args, message);
+
+    /// <summary>
+    /// 在指定集合中递归地寻找指令
+    /// </summary>
+    /// <param name="set">集合</param>
+    /// <param name="commandFound">out参数,返回找到的指令,否则为null</param>
+    /// <returns></returns>
+    public static int FindCommandInSet(ICommandSet set, ref string[] cmdParts, ref SocketMessage message, out Command commandFound)
+    {
+        //如果指令集昵称与指令第一项相同,则调用指令集内的东西
+        if (set.SetName == cmdParts[0])
+        {
+            //看看有没有更多指令集
+            if (set.ChildCommandSets.Count != 0)
+            {
+
+            }
+            else //否则就查找里面的指令
+            {
+                foreach (var cmd in set.ChildCommands)
+                {
+                    if (cmd.CommandName == cmdParts[1])
+                    {
+                        //指令昵称相同,执行指令并传参
+                        cmd.Action.Invoke(cmdParts[1..], message);
+                    }
+                }
+            }
+        }
+        //异常返回
+        commandFound = null;
+        return -1;
+    }
+
+    /// <summary>
+    /// 合并多余的参数并追加到最后一个参数后
+    /// </summary>
+    public static string[] CombineArgsParts(ref string[] strs, int index)
+    {
+        string combinedArgs = "";
+        foreach (var item in strs[index..])
+        {
+            combinedArgs += item + " ";
+        }
+        strs[index] = combinedArgs;
+        return strs;
+    }
 
 }
-// if (cmdParts.Length - 2 > cmd.ArgsCounts)
-// {
-//     for (int i = 1; i <= cmdParts.Length - 2 - cmd.argsCounts; i++)
-//     {
-//         cmdParts[1 + cmd.ArgsCounts] += " " + cmdParts[1 + cmd.argsCounts + i];
-//     }
-// }
