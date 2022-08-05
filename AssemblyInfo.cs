278bdcb16194c96091997648ea5b1c9cc685296d
Saplonily <Saplonily@outlook.com>
2022-08-05 10:54:42 +0800

    1145141919810

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyCompany("Saplonily")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("114.514.1919810")]
 [assembly: AssemblyProduct("DiscordSaladBot")]
 [assembly: AssemblyTitle("DiscordSaladBot")]
